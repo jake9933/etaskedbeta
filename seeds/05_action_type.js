762c75c95d307ce7b52391c06046f2e95863a8c0
Enrique Yepez <enriqueyt@gmail.com>
2017-09-11 01:35:21 -0500

    add like to any posts,create comments at back-end

---
+++ seeds/05_action_type.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// /* eslint-disable camelcase */
+// /* eslint-disable max-len */
+
+ exports.seed = function(knex) {
+     // Deletes ALL existing entries
+    return knex('action_types')
+        .del()
+        .then(function() {
+            const action_types = [{
+                type: 'like'
+            }];
+            return knex('action_types').insert(action_types);
+        });
+ };
